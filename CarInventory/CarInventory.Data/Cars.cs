2864b3cea22739f7786f9defaae0c29b38ff2821
manojmistry4u <manojmistry4u@gmail.com>
2019-04-16 16:32:53 +0530

    commit

---
+++ CarInventory/CarInventory.Data/Cars.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInventory.Data
+{
+    public class Cars: Entity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public override long Id { get => base.Id; set => base.Id = value; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Brand { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Model { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        [DefaultValue(false)]
+        public bool New { get; set; }
+    }
+}
---
+++ CarInventory/CarInventory.Data/EntityWithAudit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarInventory.Data
+{
+    public abstract class EntityWithAudit : Entity
+    {
+        public virtual int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public virtual int LastModifiedBy { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+    }
+}
